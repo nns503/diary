1209c62a11c681ab51c6d41476e88662e1ea8299
nns503 <73779581+nns503@users.noreply.github.com>
2024-08-14 15:21:03 +0900

    댓글 목록 조회 파라미터 형식으로 변경

--- src/main/java/backend/diary/domain/comment/controller/GetCommentController.java
+++ src/main/java/backend/diary/domain/comment/controller/GetCommentController.java
@@ -1,14 +1,16 @@
 package backend.diary.domain.comment.controller;
 
-import backend.diary.domain.comment.dto.request.GetCommentListRequest;
 import backend.diary.domain.comment.dto.response.GetCommentListResponse;
 import backend.diary.domain.comment.service.GetCommentService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
+@Validated
 @RestController
 @RequestMapping("/api/{articleId}/comment")
 public class GetCommentController {
@@ -18,9 +20,12 @@ public class GetCommentController {
     @GetMapping
     public ResponseEntity<GetCommentListResponse> getCommentList(
             @PathVariable Long articleId,
-            @RequestBody GetCommentListRequest getCommentListRequest
+            @RequestParam(defaultValue = "1") @Min(value = 1, message = "페이지 번호는 1 이상이어야 합니다.")
+            int page,
+            @RequestParam(defaultValue = "10")  @Min(value = 1, message = "페이지 사이즈는 1 이상이어야 합니다.")
+            int size
     ){
-        PageRequest pageable = PageRequest.of(getCommentListRequest.page() - 1, getCommentListRequest.size());
+        PageRequest pageable = PageRequest.of(page - 1, size);
         GetCommentListResponse response = getCommentService.getCommentList(articleId, pageable);
         return ResponseEntity.ok(response);
     }
--- src/main/java/backend/diary/domain/comment/dto/request/GetCommentListRequest.java
+++
@@ -1,7 +0,0 @@
-package backend.diary.domain.comment.dto.request;
-
-public record GetCommentListRequest(
-        int page,
-        int size
-) {
-}
